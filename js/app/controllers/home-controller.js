049309b7b424fa338220d9dd2fd6e8676dfb683c
yuhangc <yuhangc@kotei-info.com>
2017-12-26 18:05:45 +0800

    highchart

--- js/app/controllers/home-controller.js
+++ js/app/controllers/home-controller.js
@@ -254,6 +254,14 @@ define(['bootstrap/app', 'utils', 'services/usercenter-service', 'services/regul
                             chart: {
                                 type: 'column'
                             },
+                            lang: {
+                                printChart: "打印图表",
+                                downloadJPEG: "下载JPEG 图片",
+                                downloadPDF: "下载PDF文档",
+                                downloadPNG: "下载PNG 图片",
+                                downloadSVG: "下载SVG 矢量图",
+                                exportButtonTitle: "导出图片"
+                            }
                         },
 
                         title: {
